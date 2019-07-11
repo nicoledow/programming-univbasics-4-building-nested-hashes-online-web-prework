a15884f0c326cd0265f67d6af242c55b846d8f8f
Nicole Dow <nikki.dow71@gmail.com>
2019-07-11 13:57:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,20 +40,8 @@ def monopoly_with_fourth_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {},
-	    :rent_in_dollars => {}
-	  }
-	}
-end
-
-def monopoly_with_third_tier
-		monopoly = {
-	  :railroads => {
-	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {
-	        "mortgage_value" => "$100"
-	      },
+	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
